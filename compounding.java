2da32820c43be8aaf10d5afb5717be03d45374c0
13690842809 <928398631@qq.com>
2016-03-11 13:13:18 +0800

    update compounding

--- compounding.java
+++ compounding.java
@@ -6,18 +6,51 @@ public class compounding {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double sum=0;
 		Scanner s=new Scanner(System.in);
-		System.out.println("请输入本金：");
-		double money=s.nextDouble();
-		System.out.println("请输入年数:");
-		int years=s.nextInt();
-		System.out.println("请输入利率:");
-		double r=s.nextDouble();
-		for(int i=1;i<=years;i++){
-			sum=(1+r)*i*money;
-			System.out.println(i+"年后总额为"+sum);
+		System.out.println("1.复利计算   2.单利计算  3.总额反向计算本金");
+		int num=s.nextInt();
+		if(num==3){
+			double sum=0;
+			System.out.println("请输入终值：");
+			double money=s.nextDouble();
+			System.out.println("请输入年数:");
+			int years=s.nextInt();
+			System.out.println("请输入利率:");
+			double r=s.nextDouble();
+			double Single=money/(1+years*r);
+			double Double=money/(Math.pow((1+r),years));
+			System.out.println("单利现值为:"+Single);
+			System.out.println("复利现值为:"+Double);
 		}
+		
+		else{
+			double sum=0;
+			System.out.println("请输入本金：");
+			double money=s.nextDouble();
+			System.out.println("请输入年数:");
+			int years=s.nextInt();
+			System.out.println("请输入利率:");
+			double r=s.nextDouble();
+			switch(num){
+				case 1:
+					for(int i=1;i<=years;i++){
+						sum=(1+r)*i*money;
+						System.out.println(i+"年后总额为"+sum);
+					}
+					break;
+				case 2:
+					sum=money*r*years;
+					System.out.println("单利计算"+years+"年后总额为:"+sum);
+					break;
+				default:
+					System.out.println("输入有误");
+					break;
+				
+			}
+			
+		}
+		
+		
 	}
 
 }
